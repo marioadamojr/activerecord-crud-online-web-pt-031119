acc3f8e92e22af7b69247b2ca7b6d47379b6c799
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-02 01:26:37 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -88,8 +88,8 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  movie_to_destroy = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  movie_to_destroy.destory
 end
 
 def can_destroy_all_items_at_once
